0d54a6964bf2ed597107267d140611af7b78bef1
Pierre Marais <pierremarais67@gmail.com>
2026-05-01 11:19:02 +0200

    fix: use space people in asset details
    * fix: use space people in asset details
    
    * chore: update generated SQL
    
    * fix: satisfy detail panel person type check

--- web/src/lib/utils/people-utils.ts
+++ web/src/lib/utils/people-utils.ts
@@ -1,4 +1,4 @@
-import { AssetTypeEnum, type AssetFaceResponseDto } from '@immich/sdk';
+import { AssetTypeEnum } from '@immich/sdk';
 import type { Faces } from '$lib/stores/people.store';
 import { getAssetMediaUrl } from '$lib/utils';
 import { mapNormalizedRectToContent, type Rect, type Size } from '$lib/utils/container-utils';
@@ -22,7 +22,7 @@ export const getBoundingBox = (faces: Faces[], imageSize: Size): BoundingBox[] =
 };
 
 export const zoomImageToBase64 = async (
-  face: AssetFaceResponseDto,
+  face: Faces,
   assetId: string,
   assetType: AssetTypeEnum,
   photoViewer: HTMLImageElement | undefined,

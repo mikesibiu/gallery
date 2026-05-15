534615d359840e8bdac5523942b4ed9d5e164e34
Pierre Marais <pierremarais67@gmail.com>
2026-05-16 01:24:08 +0200

    feat(server): add PersonRepository.getAssignedFaceEmbeddings

--- server/src/repositories/person.repository.ts
+++ server/src/repositories/person.repository.ts
@@ -856,6 +856,19 @@ export class PersonRepository {
       .execute();
   }
 
+  @GenerateSql({ params: [DummyValue.UUID, 20] })
+  getAssignedFaceEmbeddings(personId: string, limit: number) {
+    return this.db
+      .selectFrom('asset_face')
+      .innerJoin('face_search', 'face_search.faceId', 'asset_face.id')
+      .select('face_search.embedding')
+      .where('asset_face.personId', '=', personId)
+      .where('asset_face.deletedAt', 'is', null)
+      .where('asset_face.isVisible', 'is', true)
+      .limit(limit)
+      .execute();
+  }
+
   @GenerateSql({ params: [DummyValue.UUID] })
   getRandomFace(personId: string, db: Kysely<DB> | Transaction<DB> = this.db) {
     return db

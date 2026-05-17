ba8f242ceccec1479681c7b2981c42f1c8e74b52
Pierre Marais <pierremarais67@gmail.com>
2026-05-17 19:25:13 +0200

    feat(server): add face suggestion maintenance job

--- server/src/enum.ts
+++ server/src/enum.ts
@@ -453,6 +453,7 @@ export enum ManualJobName {
   MemoryCreate = 'memory-create',
   BackupDatabase = 'backup-database',
   FaceIdentityBackfill = 'face-identity-backfill',
+  FaceSuggestionMaintenance = 'face-suggestion-maintenance',
   SharedSpacePersonMetadataBackfill = 'shared-space-person-metadata-backfill',
 }
 
@@ -737,6 +738,7 @@ export enum JobName {
   FacialRecognition = 'FacialRecognition',
   FaceIdentityBackfill = 'FaceIdentityBackfill',
   FaceIdentityMaintenanceAfterRecognition = 'FaceIdentityMaintenanceAfterRecognition',
+  FaceSuggestionMaintenance = 'FaceSuggestionMaintenance',
   PersonSuggestionScanQueueAll = 'PersonSuggestionScanQueueAll',
   PersonSuggestionScan = 'PersonSuggestionScan',
   SpacePersonSuggestionScanQueueAll = 'SpacePersonSuggestionScanQueueAll',

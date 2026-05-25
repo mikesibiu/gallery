bb27ffd123149aee7f3d0a5d4922ca4a4fca959a
Pierre Marais <pierremarais67@gmail.com>
2026-05-25 14:42:09 -0400

    fix(ci): update workflow tests for plugin methods

--- packages/sdk/src/fetch-client.ts
+++ packages/sdk/src/fetch-client.ts
@@ -3243,7 +3243,7 @@ export type UserGroupMemberResponseDto = {
 };
 export type UserGroupResponseDto = {
     /** Group color */
-    color?: Color | null;
+    color?: (UserAvatarColor) | null;
     /** Creation date */
     createdAt: string;
     /** Group ID */

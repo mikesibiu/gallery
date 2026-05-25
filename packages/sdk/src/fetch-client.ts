2ed3bab3e7b31fd7e6f8e041edcf9cffd009b3ba
Pierre Marais <pierremarais67@gmail.com>
2026-05-25 14:42:09 -0400

    fix(ci): update workflow tests for plugin methods

--- packages/sdk/src/fetch-client.ts
+++ packages/sdk/src/fetch-client.ts
@@ -3229,7 +3229,7 @@ export type UserGroupMemberResponseDto = {
 };
 export type UserGroupResponseDto = {
     /** Group color */
-    color?: Color | null;
+    color?: (UserAvatarColor) | null;
     /** Creation date */
     createdAt: string;
     /** Group ID */
@@ -9163,18 +9163,6 @@ export enum OAuthTokenEndpointAuthMethod {
 export enum AssetOrderBy {
     TakenAt = "takenAt",
     CreatedAt = "createdAt"
-}
-export enum Color {
-    Primary = "primary",
-    Pink = "pink",
-    Red = "red",
-    Yellow = "yellow",
-    Blue = "blue",
-    Green = "green",
-    Purple = "purple",
-    Orange = "orange",
-    Gray = "gray",
-    Amber = "amber"
 }
 export enum UserMetadataKey {
     Preferences = "preferences",

a02762d8caffd449bb7bbeaecbbe83c736cc554a
Pierre Marais <pierremarais67@gmail.com>
2026-05-25 06:37:02 -0400

    fix(server): guard paused face job cleanup after batch 112

--- server/src/repositories/job.repository.ts
+++ server/src/repositories/job.repository.ts
@@ -263,7 +263,7 @@ export class JobRepository {
           }
 
           job.options = action.options;
-        } else if (this.isSharedSpaceFacePipelineJob(item.name)) {
+        } else if (this.isSharedSpaceFacePipelineJob(item.name) && job.options.jobId) {
           await this.removePausedStableJob(queue, job.options.jobId);
         }
 

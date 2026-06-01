inhotel-io/inreveal

feat: add filterable timeline with FilterPanel in Spaces (#138)
* docs: add Discovery page design with interactive mockups

Design for a new /discover route that replaces the main timeline with
faceted discovery navigation: search, filtering (people, location,
camera, tags, rating, media type), sorting (capture date, upload date,
filename), and hierarchical temporal picker (year→month).

The discovery filter panel sits as a new collapsible component between
the existing nav sidebar (untouched) and the photo grid. No changes
to upstream layout files — avoids fork conflicts.

Includes six interactive HTML mockups exploring layout options,
filter panel states, and temporal navigation.

* docs: add Discovery page implementation plan (16 tasks)

Detailed TDD implementation plan covering server-side sorting
(AssetSortBy enum, dynamic orderBy), web Discovery panel with
temporal picker, filter sections (people, location, camera, tags,
rating, media type), sort dropdown, active filter chips, collapsed
state with badge indicators, and full E2E test coverage.

Cross-referenced with mockups in docs/plans/mockups/.

* docs: rewrite design for filterable timeline with generic FilterPanel

Pivoted from dedicated /discover page to a reusable FilterPanel
component. V1 ships inside Spaces (fork-only, zero upstream conflict).
Same component later reusable in albums, person views, and main
timeline.

Key changes from previous design:
- No new route — filter panel lives inside existing views
- Extends timeline endpoint with EXIF filters (Option 1 from
  endpoint comparison) instead of using search endpoint
- Search and filtering cleanly separated: filtering is exhaustive
  (timeline + WHERE clauses), search is ranked (CLIP, stays in
  global search bar)
- Person filter is single-select (server constraint)
- Generic component interface with config + data providers
- Comprehensive E2E test coverage (40+ test cases)

Also adds endpoint comparison analysis document.

* docs: update design -

open-api/typescript-sdk/src/fetch-client.ts
@@ -5895,12 +5895,13 @@ export function searchAssetStatistics({ statisticsSearchDto }: {
 /**
  * Retrieve search suggestions
  */
-export function getSearchSuggestions({ country, includeNull, lensModel, make, model, state, $type }: {
+export function getSearchSuggestions({ country, includeNull, lensModel, make, model, spaceId, state, $type }: {
     country?: string;
     includeNull?: boolean;
     lensModel?: string;
     make?: string;
     model?: string;
+    spaceId?: string;
     state?: string;
     $type: SearchSuggestionType;
 }, opts?: Oazapfts.RequestOpts) {
@@ -5913,6 +5914,7 @@ export function getSearchSuggestions({ country, includeNull, lensModel, make, mo
         lensModel,
         make,
         model,
+        spaceId,
         state,
         "type": $type
     }))}`, {
@@ -7081,19 +7083,30 @@ export function tagAssets({ id, bulkIdsDto }: {
 /**
  * Get time bucket
  */
-export function getTimeBucket({ albumId, bbox, isFavorite, isTrashed, key, order, personId, slug, spaceId, spacePersonId, tagId, timeBucket, userId, visibility, withCoordinates, withPartners, withSharedSpaces, withStacked }: {
+export function getTimeBucket({ albumId, bbox, city, country, isFavorite, isTrashed, key, make, model, order, personId, personIds, rating, slug, spaceId, spacePersonId, spacePersonIds, tagId, tagIds, takenAfter, takenBefore, timeBucket, $type, userId, visibility, withCoordinates, withPartners, withSharedSpaces, withStacked }: {
     albumId?: string;
     bbox?: string;
+    city?: string;
+    country?: string;
     isFavorite?: boolean;
     isTrashed?: boolean;
     key?: string;
+    make?: string;
+    model?: string;
     order?: AssetOrder;
     personId?: string;
+    personIds?: string[];
+    rating?: number;
     slug?: string;
     spaceId?: string;
     spacePersonId?: string;
+    spacePersonIds?: string[];
     tagId?: string;
+    tagIds?: string[];
+    takenAfter?: string;
+    takenBefore?: string;
     timeBucket: string;
+    $type?: AssetTypeEnum;
     userId?: string;
     visibility?: AssetVisibility;
     withCoordinates?: boolean;
@@ -7107,16 +7120,27 @@ export function getTimeBucket({ albumId, bbox, isFavorite, isTrashed, key, order
     }>(`/timeline/bucket${QS.query(QS.explode({
         albumId,
         bbox,
+        city,
+        country,
         isFavorite,
         isTrashed,
         key,
+        make,
+        model,
         order,
         personId,
+        personIds,
+        rating,
         slug,
         spaceId,
         spacePersonId,
+        spacePersonIds,
         tagId,
+        tagIds,
+        takenAfter,
+        takenBefore,
         timeBucket,
+        "type": $type,
         userId,
         visibility,
         withCoordinates,
@@ -7130,18 +7154,29 @@ export function getTimeBucket({ albumId, bbox, isFavorite, isTrashed, key, order
 /**
  * Get time buckets
  */
-export function getTimeBuckets({ albumId, bbox, isFavorite, isTrashed, key, order, personId, slug, spaceId, spacePersonId, tagId, userId, visibility, withCoordinates, withPartners, withSharedSpaces, withStacked }: {
+export function getTimeBuckets({ albumId, bbox, city, country, isFavorite, isTrashed, key, make, model, order, personId, personIds, rating, slug, spaceId, spacePersonId, spacePersonIds, tagId, tagIds, takenAfter, takenBefore, $type, userId, visibility, withCoordinates, withPartners, withSharedSpaces, withStacked }: {
     albumId?: string;
     bbox?: string;
+    city?: string;
+    country?: string;
     isFavorite?: boolean;
     isTrashed?: boolean;
     key?: string;
+    make?: string;
+    model?: string;
     order?: AssetOrder;
     personId?: string;
+    personIds?: string[];
+    rating?: number;
     slug?: string;
     spaceId?: string;
     spacePersonId?: string;
+    spacePersonIds?: string[];
     tagId?: string;
+    tagIds?: string[];
+    takenAfter?: string;
+    takenBefore?: string;
+    $type?: AssetTypeEnum;
     userId?: string;
     visibility?: AssetVisibility;
     withCoordinates?: boolean;
@@ -7155,15 +7190,26 @@ export function getTimeBuckets({ albumId, bbox, isFavorite, isTrashed, key, orde
     }>(`/timeline/buckets${QS.query(QS.explode({
         albumId,
         bbox,
+        city,
+        country,
         isFavorite,
         isTrashed,
         key,
+        make,
+        model,
         order,
         personId,
+        personIds,
+        rating,
         slug,
         spaceId,
         spacePersonId,
+        spacePersonIds,
         tagId,
+        tagIds,
+        takenAfter,
+        takenBefore,
+        "type": $type,
         userId,
         visibility,
         withCoordinates,

server/src/repositories/search.repository.ts
@@ -8,7 +8,7 @@ import { AssetStatus, AssetType, AssetVisibility, VectorIndex } from 'src/enum';
 import { probes } from 'src/repositories/database.repository';
 import { DB } from 'src/schema';
 import { AssetExifTable } from 'src/schema/tables/asset-exif.table';
-import { anyUuid, searchAssetBuilder, withExif } from 'src/utils/database';
+import { anyUuid, asUuid, searchAssetBuilder, withExif } from 'src/utils/database';
 import { paginationHelper } from 'src/utils/pagination';
 import { isValidInteger } from 'src/validation';
 
@@ -164,7 +164,11 @@ export interface AssetDuplicateResult {
   distance: number;
 }
 
-export interface GetStatesOptions {
+export interface SpaceScopeOptions {
+  spaceId?: string;
+}
+
+export interface GetStatesOptions extends SpaceScopeOptions {
   country?: string;
 }
 
@@ -172,17 +176,17 @@ export interface GetCitiesOptions extends GetStatesOptions {
   state?: string;
 }
 
-export interface GetCameraModelsOptions {
+export interface GetCameraModelsOptions extends SpaceScopeOptions {
   make?: string;
   lensModel?: string;
 }
 
-export interface GetCameraMakesOptions {
+export interface GetCameraMakesOptions extends SpaceScopeOptions {
   model?: string;
   lensModel?: string;
 }
 
-export interface GetCameraLensModelsOptions {
+export interface GetCameraLensModelsOptions extends SpaceScopeOptions {
   make?: string;
   model?: string;
 }
@@ -454,14 +458,14 @@ export class SearchRepository {
       .execute();
   }
 
-  async getCountries(userIds: string[]): Promise<string[]> {
-    const res = await this.getExifField('country', userIds).execute();
+  async getCountries(userIds: string[], options?: SpaceScopeOptions): Promise<string[]> {
+    const res = await this.getExifField('country', userIds, options).execute();
     return res.map((row) => row.country!);
   }
 
   @GenerateSql({ params: [[DummyValue.UUID], DummyValue.STRING] })
-  async getStates(userIds: string[], { country }: GetStatesOptions): Promise<string[]> {
-    const res = await this.getExifField('state', userIds)
+  async getStates(userIds: string[], { country, spaceId }: GetStatesOptions): Promise<string[]> {
+    const res = await this.getExifField('state', userIds, { spaceId })
       .$if(!!country, (qb) => qb.where('country', '=', country!))
       .execute();
 
@@ -469,8 +473,8 @@ export class SearchRepository {
   }
 
   @GenerateSql({ params: [[DummyValue.UUID], DummyValue.STRING, DummyValue.STRING] })
-  async getCities(userIds: string[], { country, state }: GetCitiesOptions): Promise<string[]> {
-    const res = await this.getExifField('city', userIds)
+  async getCities(userIds: string[], { country, state, spaceId }: GetCitiesOptions): Promise<string[]> {
+    const res = await this.getExifField('city', userIds, { spaceId })
       .$if(!!country, (qb) => qb.where('country', '=', country!))
       .$if(!!state, (qb) => qb.where('state', '=', state!))
       .execute();
@@ -479,8 +483,8 @@ export class SearchRepository {
   }
 
   @GenerateSql({ params: [[DummyValue.UUID], DummyValue.STRING, DummyValue.STRING] })
-  async getCameraMakes(userIds: string[], { model, lensModel }: GetCameraMakesOptions): Promise<string[]> {
-    const res = await this.getExifField('make', userIds)
+  async getCameraMakes(userIds: string[], { model, lensModel, spaceId }: GetCameraMakesOptions): Promise<string[]> {
+    const res = await this.getExifField('make', userIds, { spaceId })
       .$if(!!model, (qb) => qb.where('model', '=', model!))
       .$if(!!lensModel, (qb) => qb.where('lensModel', '=', lensModel!))
       .execute();
@@ -489,8 +493,8 @@ export class SearchRepository {
   }
 
   @GenerateSql({ params: [[DummyValue.UUID], DummyValue.STRING, DummyValue.STRING] })
-  async getCameraModels(userIds: string[], { make, lensModel }: GetCameraModelsOptions): Promise<string[]> {
-    const res = await this.getExifField('model', userIds)
+  async getCameraModels(userIds: string[], { make, lensModel, spaceId }: GetCameraModelsOptions): Promise<string[]> {
+    const res = await this.getExifField('model', userIds, { spaceId })
       .$if(!!make, (qb) => qb.where('make', '=', make!))
       .$if(!!lensModel, (qb) => qb.where('lensModel', '=', lensModel!))
       .execute();
@@ -499,8 +503,11 @@ export class SearchRepository {
   }
 
   @GenerateSql({ params: [[DummyValue.UUID], DummyValue.STRING] })
-  async getCameraLensModels(userIds: string[], { make, model }: GetCameraLensModelsOptions): Promise<string[]> {
-    const res = await this.getExifField('lensModel', userIds)
+  async getCameraLensModels(
+    userIds: string[],
+    { make, model, spaceId }: GetCameraLensModelsOptions,
+  ): Promise<string[]> {
+    const res = await this.getExifField('lensModel', userIds, { spaceId })
       .$if(!!make, (qb) => qb.where('make', '=', make!))
       .$if(!!model, (qb) => qb.where('model', '=', model!))
       .execute();
@@ -511,6 +518,7 @@ export class SearchRepository {
   private getExifField<K extends 'city' | 'state' | 'country' | 'make' | 'model' | 'lensModel'>(
     field: K,
     userIds: string[],
+    options?: SpaceScopeOptions,
   ) {
     return this.db
       .selectFrom('asset_exif')
@@ -520,6 +528,16 @@ export class SearchRepository {
       .where('ownerId', '=', anyUuid(userIds))
       .where('visibility', '=', AssetVisibility.Timeline)
       .where('deletedAt', 'is', null)
-      .where(field, 'is not', null);
+      .where(field, 'is not', null)
+      .$if(!!options?.spaceId, (qb) =>
+        qb.where((eb) =>
+          eb.exists(
+            eb
+              .selectFrom('shared_space_asset')
+              .whereRef('shared_space_asset.assetId', '=', 'asset.id')
+              .where('shared_space_asset.spaceId', '=', asUuid(options!.spaceId!)),
+          ),
+        ),
+      );
   }
 }

mikesibiu/gallery

packages/sdk/src/fetch-client.ts
@@ -5612,12 +5612,13 @@ export function searchAssetStatistics({ statisticsSearchDto }: {
 /**
  * Retrieve search suggestions
  */
-export function getSearchSuggestions({ country, includeNull, lensModel, make, model, state, $type }: {
+export function getSearchSuggestions({ country, includeNull, lensModel, make, model, spaceId, state, $type }: {
     country?: string;
     includeNull?: boolean;
     lensModel?: string;
     make?: string;
     model?: string;
+    spaceId?: string;
     state?: string;
     $type: SearchSuggestionType;
 }, opts?: Oazapfts.RequestOpts) {
@@ -5630,6 +5631,7 @@ export function getSearchSuggestions({ country, includeNull, lensModel, make, mo
         lensModel,
         make,
         model,
+        spaceId,
         state,
         "type": $type
     }))}`, {
@@ -6373,22 +6375,36 @@ export function tagAssets({ id, bulkIdsDto }: {
 /**
  * Get time bucket
  */
-export function getTimeBucket({ albumId, bbox, isFavorite, isTrashed, key, order, orderBy, personId, slug, tagId, timeBucket, userId, visibility, withCoordinates, withPartners, withStacked }: {
+export function getTimeBucket({ albumId, bbox, city, country, isFavorite, isTrashed, key, make, model, order, orderBy, personId, personIds, rating, slug, spaceId, spacePersonId, spacePersonIds, tagId, tagIds, takenAfter, takenBefore, timeBucket, $type, userId, visibility, withCoordinates, withPartners, withSharedSpaces, withStacked }: {
     albumId?: string;
     bbox?: string;
+    city?: string;
+    country?: string;
     isFavorite?: boolean;
     isTrashed?: boolean;
     key?: string;
+    make?: string;
+    model?: string;
     order?: AssetOrder;
     orderBy?: AssetOrderBy;
     personId?: string;
+    personIds?: string[];
+    rating?: number;
     slug?: string;
+    spaceId?: string;
+    spacePersonId?: string;
+    spacePersonIds?: string[];
     tagId?: string;
+    tagIds?: string[];
+    takenAfter?: string;
+    takenBefore?: string;
     timeBucket: string;
+    $type?: AssetTypeEnum;
     userId?: string;
     visibility?: AssetVisibility;
     withCoordinates?: boolean;
     withPartners?: boolean;
+    withSharedSpaces?: boolean;
     withStacked?: boolean;
 }, opts?: Oazapfts.RequestOpts) {
     return oazapfts.ok(oazapfts.fetchJson<{
@@ -6397,19 +6413,33 @@ export function getTimeBucket({ albumId, bbox, isFavorite, isTrashed, key, order
     }>(`/timeline/bucket${QS.query(QS.explode({
         albumId,
         bbox,
+        city,
+        country,
         isFavorite,
         isTrashed,
         key,
+        make,
+        model,
         order,
         orderBy,
         personId,
+        personIds,
+        rating,
         slug,
+        spaceId,
+        spacePersonId,
+        spacePersonIds,
         tagId,
+        tagIds,
+        takenAfter,
+        takenBefore,
         timeBucket,
+        "type": $type,
         userId,
         visibility,
         withCoordinates,
         withPartners,
+        withSharedSpaces,
         withStacked
     }))}`, {
         ...opts
@@ -6418,21 +6448,35 @@ export function getTimeBucket({ albumId, bbox, isFavorite, isTrashed, key, order
 /**
  * Get time buckets
  */
-export function getTimeBuckets({ albumId, bbox, isFavorite, isTrashed, key, order, orderBy, personId, slug, tagId, userId, visibility, withCoordinates, withPartners, withStacked }: {
+export function getTimeBuckets({ albumId, bbox, city, country, isFavorite, isTrashed, key, make, model, order, orderBy, personId, personIds, rating, slug, spaceId, spacePersonId, spacePersonIds, tagId, tagIds, takenAfter, takenBefore, $type, userId, visibility, withCoordinates, withPartners, withSharedSpaces, withStacked }: {
     albumId?: string;
     bbox?: string;
+    city?: string;
+    country?: string;
     isFavorite?: boolean;
     isTrashed?: boolean;
     key?: string;
+    make?: string;
+    model?: string;
     order?: AssetOrder;
     orderBy?: AssetOrderBy;
     personId?: string;
+    personIds?: string[];
+    rating?: number;
     slug?: string;
+    spaceId?: string;
+    spacePersonId?: string;
+    spacePersonIds?: string[];
     tagId?: string;
+    tagIds?: string[];
+    takenAfter?: string;
+    takenBefore?: string;
+    $type?: AssetTypeEnum;
     userId?: string;
     visibility?: AssetVisibility;
     withCoordinates?: boolean;
     withPartners?: boolean;
+    withSharedSpaces?: boolean;
     withStacked?: boolean;
 }, opts?: Oazapfts.RequestOpts) {
     return oazapfts.ok(oazapfts.fetchJson<{
@@ -6441,18 +6485,32 @@ export function getTimeBuckets({ albumId, bbox, isFavorite, isTrashed, key, orde
     }>(`/timeline/buckets${QS.query(QS.explode({
         albumId,
         bbox,
+        city,
+        country,
         isFavorite,
         isTrashed,
         key,
+        make,
+        model,
         order,
         orderBy,
         personId,
+        personIds,
+        rating,
         slug,
+        spaceId,
+        spacePersonId,
+        spacePersonIds,
         tagId,
+        tagIds,
+        takenAfter,
+        takenBefore,
+        "type": $type,
         userId,
         visibility,
         withCoordinates,
         withPartners,
+        withSharedSpaces,
         withStacked
     }))}`, {
         ...opts

server/src/repositories/search.repository.ts
@@ -6,7 +6,7 @@ import { AssetStatus, AssetType, AssetVisibility, VectorIndex } from 'src/enum';
 import { probes } from 'src/repositories/database.repository';
 import { DB } from 'src/schema';
 import { AssetExifTable } from 'src/schema/tables/asset-exif.table';
-import { anyUuid, searchAssetBuilder, withExifInner } from 'src/utils/database';
+import { anyUuid, asUuid, searchAssetBuilder, withExifInner } from 'src/utils/database';
 import { paginationHelper } from 'src/utils/pagination';
 import { isValidInteger } from 'src/validation';
 
@@ -160,7 +160,11 @@ export interface AssetDuplicateResult {
   distance: number;
 }
 
-export interface GetStatesOptions {
+export interface SpaceScopeOptions {
+  spaceId?: string;
+}
+
+export interface GetStatesOptions extends SpaceScopeOptions {
   country?: string;
 }
 
@@ -168,17 +172,17 @@ export interface GetCitiesOptions extends GetStatesOptions {
   state?: string;
 }
 
-export interface GetCameraModelsOptions {
+export interface GetCameraModelsOptions extends SpaceScopeOptions {
   make?: string;
   lensModel?: string;
 }
 
-export interface GetCameraMakesOptions {
+export interface GetCameraMakesOptions extends SpaceScopeOptions {
   model?: string;
   lensModel?: string;
 }
 
-export interface GetCameraLensModelsOptions {
+export interface GetCameraLensModelsOptions extends SpaceScopeOptions {
   make?: string;
   model?: string;
 }
@@ -441,14 +445,14 @@ export class SearchRepository {
       .execute();
   }
 
-  async getCountries(userIds: string[]): Promise<string[]> {
-    const res = await this.getExifField('country', userIds).execute();
+  async getCountries(userIds: string[], options?: SpaceScopeOptions): Promise<string[]> {
+    const res = await this.getExifField('country', userIds, options).execute();
     return res.map((row) => row.country!);
   }
 
   @GenerateSql({ params: [[DummyValue.UUID], DummyValue.STRING] })
-  async getStates(userIds: string[], { country }: GetStatesOptions): Promise<string[]> {
-    const res = await this.getExifField('state', userIds)
+  async getStates(userIds: string[], { country, spaceId }: GetStatesOptions): Promise<string[]> {
+    const res = await this.getExifField('state', userIds, { spaceId })
       .$if(!!country, (qb) => qb.where('country', '=', country!))
       .execute();
 
@@ -456,8 +460,8 @@ export class SearchRepository {
   }
 
   @GenerateSql({ params: [[DummyValue.UUID], DummyValue.STRING, DummyValue.STRING] })
-  async getCities(userIds: string[], { country, state }: GetCitiesOptions): Promise<string[]> {
-    const res = await this.getExifField('city', userIds)
+  async getCities(userIds: string[], { country, state, spaceId }: GetCitiesOptions): Promise<string[]> {
+    const res = await this.getExifField('city', userIds, { spaceId })
       .$if(!!country, (qb) => qb.where('country', '=', country!))
       .$if(!!state, (qb) => qb.where('state', '=', state!))
       .execute();
@@ -466,8 +470,8 @@ export class SearchRepository {
   }
 
   @GenerateSql({ params: [[DummyValue.UUID], DummyValue.STRING, DummyValue.STRING] })
-  async getCameraMakes(userIds: string[], { model, lensModel }: GetCameraMakesOptions): Promise<string[]> {
-    const res = await this.getExifField('make', userIds)
+  async getCameraMakes(userIds: string[], { model, lensModel, spaceId }: GetCameraMakesOptions): Promise<string[]> {
+    const res = await this.getExifField('make', userIds, { spaceId })
       .$if(!!model, (qb) => qb.where('model', '=', model!))
       .$if(!!lensModel, (qb) => qb.where('lensModel', '=', lensModel!))
       .execute();
@@ -476,8 +480,8 @@ export class SearchRepository {
   }
 
   @GenerateSql({ params: [[DummyValue.UUID], DummyValue.STRING, DummyValue.STRING] })
-  async getCameraModels(userIds: string[], { make, lensModel }: GetCameraModelsOptions): Promise<string[]> {
-    const res = await this.getExifField('model', userIds)
+  async getCameraModels(userIds: string[], { make, lensModel, spaceId }: GetCameraModelsOptions): Promise<string[]> {
+    const res = await this.getExifField('model', userIds, { spaceId })
       .$if(!!make, (qb) => qb.where('make', '=', make!))
       .$if(!!lensModel, (qb) => qb.where('lensModel', '=', lensModel!))
       .execute();
@@ -486,8 +490,11 @@ export class SearchRepository {
   }
 
   @GenerateSql({ params: [[DummyValue.UUID], DummyValue.STRING] })
-  async getCameraLensModels(userIds: string[], { make, model }: GetCameraLensModelsOptions): Promise<string[]> {
-    const res = await this.getExifField('lensModel', userIds)
+  async getCameraLensModels(
+    userIds: string[],
+    { make, model, spaceId }: GetCameraLensModelsOptions,
+  ): Promise<string[]> {
+    const res = await this.getExifField('lensModel', userIds, { spaceId })
       .$if(!!make, (qb) => qb.where('make', '=', make!))
       .$if(!!model, (qb) => qb.where('model', '=', model!))
       .execute();
@@ -495,7 +502,11 @@ export class SearchRepository {
     return res.map((row) => row.lensModel!);
   }
 
-  private getExifField(field: 'city' | 'state' | 'country' | 'make' | 'model' | 'lensModel', userIds: string[]) {
+  private getExifField<K extends 'city' | 'state' | 'country' | 'make' | 'model' | 'lensModel'>(
+    field: K,
+    userIds: string[],
+    options?: SpaceScopeOptions,
+  ) {
     return this.db
       .selectFrom('asset_exif')
       .select(field)
@@ -505,6 +516,16 @@ export class SearchRepository {
       .where('visibility', '=', AssetVisibility.Timeline)
       .where('deletedAt', 'is', null)
       .where(field, 'is not', null)
-      .where(field, '!=', '');
+      .where(field, '!=', '')
+      .$if(!!options?.spaceId, (qb) =>
+        qb.where((eb) =>
+          eb.exists(
+            eb
+              .selectFrom('shared_space_asset')
+              .whereRef('shared_space_asset.assetId', '=', 'asset.id')
+              .where('shared_space_asset.spaceId', '=', asUuid(options!.spaceId!)),
+          ),
+        ),
+      );
   }
 }
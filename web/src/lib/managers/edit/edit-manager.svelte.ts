inhotel-io/inreveal

feat: add video trimming to editor (#191)
* feat: add Trim action to edit DTO with TrimParameters

* feat: add duration field to getForEdit query for video trim validation

* feat: add duration string/seconds conversion utilities for video trim

* feat: restructure editAsset validation to support video trim

* feat: restore original duration when removing trim edits

* feat: add MediaRepository.trim() and extractFrame() for video editing

* feat: prefer edited encoded video in getForVideo query

* feat: add video trim branch to edit job handler

* chore: regenerate OpenAPI specs for Trim action

* feat: add TrimManager with video element sync and constrained playback

* feat: split canEdit for video support, select trim tool for videos

* test: add TrimManager unit tests

* chore: fix server lint, formatting, and type errors for video trim

* chore: fix web lint, formatting, and type errors for video trim

* test: add E2E API tests for video trimming

Cover trim success, re-trim, undo, and rejection cases (image asset,
endTime exceeding duration, full-duration no-op, short video, mixed
spatial+trim edits, auth, access).

* feat: wire video element ref from viewer to trim editor

Pass HTMLVideoElement reference through AssetViewer → VideoWrapperViewer
→ VideoNativeViewer chain to EditorPanel. EditorPanel passes it to
TrimManager when trim tool is active. Native video controls hidden
during editing to prevent spacebar conflicts.

* docs: add video trimming design, plan, and interactive mockup

* fix(ci): formatting, SQL query docs, and E2E lint for video trim

- Prettier formatting on mockup HTML, asset.service.spec, media.service
- Regenerate SQL query docs for getForEdit/getForVideo changes
- Remove unused asBearerAuth import in E2E test

* fix(ci): restructure E2E trim tests to avoid shared state

Each mutation test gets a fresh video asset. Rejection tests run
on untrimmed assets. Avoids async job modifying asset.duration
between sequential tests.

* fix(ci): remo

web/src/lib/managers/edit/edit-manager.svelte.ts
@@ -1,9 +1,12 @@
 import TransformTool from '$lib/components/asset-viewer/editor/transform-tool/transform-tool.svelte';
+import TrimTool from '$lib/components/asset-viewer/editor/trim-tool/trim-tool.svelte';
 import { transformManager } from '$lib/managers/edit/transform-manager.svelte';
+import { trimManager } from '$lib/managers/edit/trim-manager.svelte';
 import { eventManager } from '$lib/managers/event-manager.svelte';
 import { waitForWebsocketEvent } from '$lib/stores/websocket';
 import { getFormatter } from '$lib/utils/i18n';
 import {
+  AssetTypeEnum,
   editAsset,
   getAssetInfo,
   removeAssetEdits,
@@ -11,7 +14,7 @@ import {
   type AssetResponseDto,
 } from '@immich/sdk';
 import { ConfirmModal, modalManager, toastManager } from '@immich/ui';
-import { mdiCropRotate } from '@mdi/js';
+import { mdiContentCut, mdiCropRotate } from '@mdi/js';
 import type { Component } from 'svelte';
 
 export type EditAction = AssetEditsCreateDto['edits'][number];
@@ -28,6 +31,7 @@ export interface EditToolManager {
 
 export enum EditToolType {
   Transform = 'transform',
+  Trim = 'trim',
 }
 
 export interface EditTool {
@@ -44,6 +48,12 @@ export class EditManager {
       icon: mdiCropRotate,
       component: TransformTool,
       manager: transformManager,
+    },
+    {
+      type: EditToolType.Trim,
+      icon: mdiContentCut,
+      component: TrimTool,
+      manager: trimManager,
     },
   ];
 
@@ -123,7 +133,7 @@ export class EditManager {
   async applyEdits(): Promise<boolean> {
     this.isApplyingEdits = true;
 
-    const edits = this.tools.flatMap((tool) => tool.manager.edits);
+    const edits = this.selectedTool ? this.selectedTool.manager.edits : [];
     if (!this.currentAsset) {
       return false;
     }
@@ -133,7 +143,8 @@ export class EditManager {
 
     try {
       // Setup the websocket listener before sending the edit request
-      const editCompleted = waitForWebsocketEvent('AssetEditReadyV1', (event) => event.asset.id === assetId, 10_000);
+      const timeout = this.currentAsset?.type === AssetTypeEnum.Video ? 30_000 : 10_000;
+      const editCompleted = waitForWebsocketEvent('AssetEditReadyV1', (event) => event.asset.id === assetId, timeout);
 
       await (edits.length === 0
         ? removeAssetEdits({ id: assetId })

mikesibiu/gallery

web/src/lib/managers/edit/edit-manager.svelte.ts
@@ -1,9 +1,12 @@
 import TransformTool from '$lib/components/asset-viewer/editor/transform-tool/TransformTool.svelte';
+import TrimTool from '$lib/components/asset-viewer/editor/trim-tool/trim-tool.svelte';
 import { transformManager } from '$lib/managers/edit/transform-manager.svelte';
+import { trimManager } from '$lib/managers/edit/trim-manager.svelte';
 import { eventManager } from '$lib/managers/event-manager.svelte';
 import { waitForWebsocketEvent } from '$lib/stores/websocket';
 import { getFormatter } from '$lib/utils/i18n';
 import {
+  AssetTypeEnum,
   editAsset,
   getAssetInfo,
   removeAssetEdits,
@@ -11,7 +14,7 @@ import {
   type AssetResponseDto,
 } from '@immich/sdk';
 import { ConfirmModal, modalManager, toastManager } from '@immich/ui';
-import { mdiCropRotate } from '@mdi/js';
+import { mdiContentCut, mdiCropRotate } from '@mdi/js';
 import type { Component } from 'svelte';
 
 export type EditAction = AssetEditsCreateDto['edits'][number];
@@ -28,6 +31,7 @@ export interface EditToolManager {
 
 export enum EditToolType {
   Transform = 'transform',
+  Trim = 'trim',
 }
 
 export interface EditTool {
@@ -44,6 +48,12 @@ export class EditManager {
       icon: mdiCropRotate,
       component: TransformTool,
       manager: transformManager,
+    },
+    {
+      type: EditToolType.Trim,
+      icon: mdiContentCut,
+      component: TrimTool,
+      manager: trimManager,
     },
   ];
 
@@ -123,7 +133,7 @@ export class EditManager {
   async applyEdits(): Promise<boolean> {
     this.isApplyingEdits = true;
 
-    const edits = this.tools.flatMap((tool) => tool.manager.edits);
+    const edits = this.selectedTool ? this.selectedTool.manager.edits : [];
     if (!this.currentAsset) {
       return false;
     }
@@ -133,7 +143,8 @@ export class EditManager {
 
     try {
       // Setup the websocket listener before sending the edit request
-      const editCompleted = waitForWebsocketEvent('AssetEditReadyV2', (event) => event.asset.id === assetId, 10_000);
+      const timeout = this.currentAsset?.type === AssetTypeEnum.Video ? 30_000 : 10_000;
+      const editCompleted = waitForWebsocketEvent('AssetEditReadyV2', (event) => event.asset.id === assetId, timeout);
 
       await (edits.length === 0
         ? removeAssetEdits({ id: assetId })